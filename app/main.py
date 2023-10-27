0761a5671159a521accc57213061d17d0e450381
David Gomez <david.gomez@landbot.io>
2023-10-27 17:03:43 +0200

    request body

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 from enum import Enum
-
+from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
@@ -22,6 +29,21 @@ async def read_item(skip: int = 0, limit: int = 10):
     fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
     
     return fake_items_db[skip : skip + limit]
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item, q: Union[str, None] = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
 
 @app.get("/users/me")
 async def read_user_me():
