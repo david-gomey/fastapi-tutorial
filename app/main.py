aeca5225683a8751000a21f75b1ab6b821dd68e5
David Gomez <david.gomez@landbot.io>
2023-10-27 16:52:27 +0200

    query params

--- app/main.py
+++ app/main.py
@@ -17,6 +17,11 @@ def read_root():
 async def read_item(item_id: str):
     return {"item_id": item_id}
 
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+    
+    return fake_items_db[skip : skip + limit]
 
 @app.get("/users/me")
 async def read_user_me():
